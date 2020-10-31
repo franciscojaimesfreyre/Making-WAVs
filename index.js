81544176506c2b84544a90c2f52036e8010e7bf6
Francisco J Freyre <francisco@Franciscos-MBP.fibertel.com.ar>
2020-10-31 04:41:21 -0300

    normalization, rms, dbs

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const B = require('arcsecond-binary');
 const C = require('construct-js');
 const fs = require('fs');
 const path = require('path');
+const d3nLine = require('d3node-linechart');
+const output = require('d3node-output');
 
-const file = fs.readFileSync(path.join(__dirname, './test.wav'));
+const file = fs.readFileSync(path.join(__dirname, './file_example.wav'));
 
 const riffChunkSize = B.u32LE.chain(size => {
   if (size !== file.length - 8) {
-    return A.fail(`Invalid file size: ${file.length}. Expected ${size}`);
+    return A.fail(`Invalid file size: ${file.length}. Expected ${size + 8}`);
   }
   return A.succeedWith(size);
 });
@@ -54,6 +56,23 @@ const fmtSubChunk = A.coroutine(function* () {
   return fmtChunkData;
 });
 
+// const factSubChunk = A.coroutine(function* () {
+//   const empty1 = yield A.anyChar;
+//   const empty2 = yield A.anyChar;
+//   const id = yield A.str('fact');
+//   const subChunkSize = yield B.u32LE;
+//   const audioFormat = yield B.u32LE;
+
+//   const factChunkData = {
+//     id,
+//     subChunkSize,
+//     audioFormat
+//   };
+//   return factChunkData;
+// });
+
+
+
 const dataSubChunk = A.coroutine(function* () {
   const id = yield A.str('data');
   const size = yield B.u32LE;
@@ -92,55 +111,57 @@ const parser = A.sequenceOf([
   riffChunk,
   fmtSubChunk,
   dataSubChunk,
-  A.endOfInput
-]).map(([riffChunk, fmtSubChunk, dataSubChunk]) => ({
+  //A.endOfInput
+]).map(([riffChunk, fmtSubChunk,dataSubChunk]) => ({
   riffChunk,
   fmtSubChunk,
   dataSubChunk
 }));
 
-const output = parser.run(file.buffer);
-if (output.isError) {
-  throw new Error(output.error);
+const myOutput = parser.run(file.buffer);
+if (myOutput.isError) {
+  console.log(myOutput.error);
+  throw new Error(myOutput.error);
+}
+console.log(myOutput.result.dataSubChunk.channelData);
+
+
+const dataArray = peakNormalize(myOutput.result.dataSubChunk.channelData[0],32768);
+// const dataArray = myOutput.result.dataSubChunk.channelData[0];
+
+//TODO: calculate for each channel and sum them up(?)
+console.log("DBs: ",calculateRMSDbs(dataArray));
+drawLineChart(dataArray);
+
+
+function calculateRMSDbs(dataArray) {
+  let sum = 0;
+  dataArray.forEach(value => {
+    let sample = value / 32768;
+    sum += (sample * sample);
+  });
+  const rms = Math.sqrt(sum / (dataArray.length / 2));
+  const db = 20 * Math.log10(rms);
+  return db;
 }
 
-const riffChunkStruct = C.Struct('riffChunk')
-  .field('magic', C.RawString('RIFF'))
-  .field('size', C.U32LE(0))
-  .field('fmtName', C.RawString('WAVE'));
-
-const fmtSubChunkStruct = C.Struct('fmtSubChunk')
-  .field('id', C.RawString('fmt '))
-  .field('subChunk1Size', C.U32LE(0))
-  .field('audioFormat', C.U16LE(1))
-  .field('numChannels', C.U16LE(1))
-  .field('sampleRate', C.U32LE(44100))
-  .field('byteRate', C.U32LE(44100 * 2))
-  .field('blockAlign', C.U16LE(2))
-  .field('bitsPerSample', C.U16LE(16));
-const totalSubChunkSize = fmtSubChunkStruct.computeBufferSize();
-fmtSubChunkStruct.get('subChunk1Size').set(totalSubChunkSize - 8);
-
-const dataSubChunkStruct = C.Struct('dataSubChunk')
-  .field('id', C.RawString('data'))
-  .field('size', C.U32LE(0))
-  .field('data', C.S16LEs([0]));
-
-const soundData = [];
-let isUp = true;
-for (let i = 0; i < 44100; i++) {
-  if (i % 100 === 0) {
-    isUp = !isUp;
-  }
-  const sampleValue = isUp ? 16383 : -16383;
-  soundData[i] = sampleValue;
+function drawLineChart(dataArray) {
+  const data = dataArray.map((value, index) => { return { key: index, value: value / 32768 }; });
+  const line = d3nLine({ data: data });
+  output('./examples/output', line, { width: 960, height: 550 });
 }
-dataSubChunkStruct.get('data').set(soundData);
-dataSubChunkStruct.get('size').set(soundData.length * 2);
 
-const fileStruct = C.Struct('waveFile')
-  .field('riffChunk', riffChunkStruct)
-  .field('fmtSubChunk', fmtSubChunkStruct)
-  .field('dataSubChunk', dataSubChunkStruct);
+function peak(audioChannelData) {
+  const audioChannelDataAbs = audioChannelData.map(value => Math.abs(value));
+  let max = audioChannelDataAbs[0];
+  audioChannelDataAbs.forEach(value => {
+    if(value > max) max = value;
+  });
+  return max;
+}
 
-fs.writeFileSync(path.join(__dirname, './new.wav'), fileStruct.toBuffer());
+function peakNormalize(audioChannelData, myCeroDb) {
+  const mypeak = peak(audioChannelData);
+  const factor = myCeroDb / mypeak;
+  return audioChannelData.map(value => value * factor);
+}
